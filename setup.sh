85da28da6346bd55bbfde70a536bc83a2bed5e76
FWachter <wachterfreddy@gmail.com>
2017-09-27 12:26:28 -0400

    Updated setup scripts

--- setup.sh
+++ setup.sh
@@ -8,18 +8,18 @@
 # Create ROS Swerve Workspace and Aliases
 #
 
-mkdir Swerve
+mkdir ~/Swerve
 cd ~/Swerve
 
 # Crete Swerve ROS workspace and get swerve meta-package
-mkdir swerve_ws swerve_ws/src
+mkdir swerve_ws swerve_ws/src swerve_resources
 
 cd ~/swerve_ws/src
 git clone https://github.com/SwerveRoboticSystems/swerve.git
 echo "alias wsroot=\'cd ~/Swerve/swerve_ws\'" >> ~/.bashrc
 cd ~/Swerve
 
-mv ~/swerve_resources ~/Swerve/
+mv ~/swerve_resources/ ~/Swerve/swerve_resources/
 
 # Get Swerve resources repository
 bash swerve_resources/create_package/setup.sh
