58660ad0c28a481999109da27ef5346dcef65650
Frederick Wachter <wachterfreddy@gmail.com>
2017-11-26 09:36:01 -0500

    Fixed auto download of packages

--- installations/ros_pkgs_install.sh
+++ installations/ros_pkgs_install.sh
@@ -9,9 +9,9 @@ echo "[SWERVE] Installing ROS Supporting Packages for Swerve"
 sudo apt-get install ros-$1-ros-control -y # ros control
 sudo apt-get install ros-$1-ros-controllers -y # ros controllers
 sudo apt-get install ros-$1-gazebo-ros-control -y # gazebo ros control
-sudo apt-get install ros-$1-sick-tim # sick tim
-sudo apt-get install ros-$1-openni-camera # gazebo depth camera
-sudo apt-get install ros-$1-openni-launch # gazebo depth camera
-sudo apt-get install libopenni0 libopenni-dev # depth camera supporting packages
+sudo apt-get install ros-$1-sick-tim -y # sick tim
+sudo apt-get install ros-$1-openni-camera -y # gazebo depth camera
+sudo apt-get install ros-$1-openni-launch -y # gazebo depth camera
+sudo apt-get install libopenni0 libopenni-dev -y # depth camera supporting packages
 
 
